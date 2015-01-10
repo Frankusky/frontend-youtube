a2b50efaf252bb508ea2c4230b1240b5962d7f42
Frank Sam Hu <fsh1991@hotmail.com>
2015-01-09 18:22:48 -0600

    Now channel video list works

--- js/script.js
+++ js/script.js
@@ -7,41 +7,15 @@ $(document).ready(function() {
 	var likeCount = new Array; //List that save the likeCount data
 	var alphabetical = new Array; //List that save the title data
 	var images = new Array; //List that save the images data
-
-	var viewCountSorted = new Array;
-	var likeCountSorted = new Array;
-	var alphabeticalSorted = new Array;
-	var imagesSorted = new Array;
-
-	var sortedList = new Array; //List that will make a copy of a list and will sort it
-	var elementsIndex = new Array; //List that will save the order of the list to show elements in the right order
-	
-	var jsonObject;
-
-	//Function that will sort a list and save the original index
-	function sortingElements(listName){
-		sortedList = listName.slice(0).sort();
-		for (var i=0; i<listName.length;i++){
-			elementsIndex.push(listName.indexOf(sortedList[i]));
-		};
-		for (var i = 0; i <elementsIndex.length; i++) {
-			viewCountSorted.push(viewCount[elementsIndex[i]]);
-			likeCountSorted.push(likeCount[elementsIndex[i]]);
-			alphabeticalSorted.push(alphabetical[elementsIndex[i]]);
-			imagesSorted.push(images[elementsIndex[i]]);
-		};
-
-		
-
-	};
-
+	var idArray = new Array; //List that will save the id's of each video
+	var jsonObject = new Array; //Json
 
 	//First ajax call to retrieve all video ID urls
 	$.ajax({
 		url : "https://www.googleapis.com/youtube/v3/search?key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo&channelId=UCZJ7m7EnCNodqnu5SAtg8eQ&part=snippet&maxResults="+maxResults+"&format=json",
 		dataType : "jsonp",
 		success : function(parsed_json) {
-			for (var i = 0; i <= 9; i++) {
+			for (var i = 0; i < maxResults; i++) {
 				if (i==9){
 					stringUrl = stringUrl + parsed_json["items"][i]["id"]["videoId"];
 				}else{
@@ -49,8 +23,6 @@ $(document).ready(function() {
 				};		
 			};
 			finalUrl = "https://www.googleapis.com/youtube/v3/videos?part=snippet%2C+statistics&id="+stringUrl+"&maxResults=10&key=AIzaSyCZIdvJkrCNDjswEeRtMTbN6B9yEiz2pEo";
-			console.log(finalUrl);
-
 			//Second ajax call to retrieve every data of the videos
 			$.ajax({
 				url: finalUrl,
@@ -61,19 +33,45 @@ $(document).ready(function() {
 						likeCount.push(data["items"][e]["statistics"]["likeCount"]);
 						alphabetical.push(data["items"][e]["snippet"]["title"]);
 						images.push(data["items"][e]["snippet"]["thumbnails"]["default"]["url"]);
+						idArray.push(data["items"][e]["id"]);
 					};
-					var knockoutYoutube = function(){
-						//this.viewCountLess = function(){
-							sortingElements(viewCount);
-							
-							jsonObject = [{title:alphabeticalSorted, viewed:viewCountSorted, likes:likeCountSorted, videoImage:imagesSorted}];
-							this.jsonModel =  ko.observableArray(jsonObject);
-							
-						//};
+					for (var a = 0; a <maxResults; a++) {
+						jsonObject.push(({name:alphabetical[a], images:images[a], view:viewCount[a], like:likeCount[a], ids:idArray[a]}))
 					};
-					ko.applyBindings(new knockoutYoutube);
-				}
-			});
-		}
-	});
-});
+					console.log(idArray)
+					var knockoutYoutube = function(items){
+
+						
+						this.items = ko.observableArray(items);
+						this.jsonModel =  ko.observableArray(jsonObject);
+						this.gridOptions = {
+							data: this.items,
+							rowTemplate: "rowTmpl",
+							useKOTemplates: true,
+							height: 430,
+							columns: [ 
+								{
+									title: "Name"
+								},
+								{
+									title: "Image"   
+								},
+								{
+									title: "View Count"
+								},
+								{
+									title: "Like Count"
+								},
+							],
+							pageable: {
+							pageSize: 3,
+							buttonCount: 1
+							},
+						};
+					};
+					ko.applyBindings(new knockoutYoutube(jsonObject));
+				}//ajax on success call
+			});//second ajax call
+		}//ajax on success call
+	}); //First ajax call
+}); //Document ready
